d662e45a71fd8a2f805c52bf038210070479a44c
Hoe <phamthikhanhhoa06@gmail.com>
2026-04-18 15:26:33 +0700

    Add DichVu GiatUi DiChuyen

---
+++ ql_ks/ViewModels/GiatUi_RelayCommand.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ql_ks.ViewModels
+{
+    public class GiatUi_RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public GiatUi_RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    // Phiên bản generic (tùy chọn, có thể không cần nếu chỉ dùng object)
+    public class GiatUi_RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public GiatUi_RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null) return true;
+
+            if (parameter == null)
+                return _canExecute(default(T));
+
+            return _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter == null)
+                _execute(default(T));
+            else
+                _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ ql_ks/Views/UC_DichVuGiatUi.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ql_ks.ViewModels;
+
+namespace ql_ks.Views
+{
+    /// <summary>
+    /// Interaction logic for UC_DichVuGiatUi.xaml
+    /// </summary>
+    public partial class UC_DichVuGiatUi : UserControl
+    {
+        public UC_DichVuGiatUi()
+        {
+            InitializeComponent();
+            DataContext = new DichVuGiatUiViewModel();
+
+            // Lưu reference để gọi Xóa với parameter
+            if (DataContext is DichVuGiatUiViewModel vm)
+            {
+                this.Loaded += (s, e) =>
+                {
+                    // Có thể thêm event handler tại đây nếu cần
+                };
+            }
+        }
+
+        // Event handler nếu cần xử lý nút ngoài DataGrid
+        private void BtnXoa_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is LuotGiatDaChonVM item)
+            {
+                var vm = DataContext as DichVuGiatUiViewModel;
+                vm?.XoaKhoiGio(item);
+            }
+        }
+    }
+}
